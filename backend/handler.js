3169572fe58fa043294105726c1de484c229d5ea
Bruno Bonando <bruno@bonandos.com>
2017-11-08 04:19:20 -0200

    Add CORS

--- backend/handler.js
+++ backend/handler.js
@@ -4,6 +4,10 @@ module.exports.hello = (event, context, callback) => {
   console.log('Running lambda function')
   const response = {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Credentials" : true
+    },
     body: JSON.stringify({
       message: 'Bruno Bonando says hello!'
     }),
